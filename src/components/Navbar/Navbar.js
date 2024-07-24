5071bcd97b5bdc371f30543b830f3ae6a3c9ca62
sareenAhamed <sareenahamed40@gmail.com>
2024-07-24 18:38:05 +0530

    Navbar updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../Assets/logo.png'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='container'>
+      <nav className='navbar'>
+        <div className='nav-logo'>
+          <img src={logo} alt=''/>
+        </div>
+        <div className='nav'>
+          <ul className='list-inline'>
+            <li className='list-inline-item'><a className='nav-link' href='#'>Home</a></li>
+            <li className='list-inline-item'><a className='nav-link' href='#'>About</a></li>
+            <li className='list-inline-item'><a className='nav-link' href='#'>Skills</a></li>
+            <li className='list-inline-item'><a className='nav-link' href='#'>Projects</a></li>
+            <li className='list-inline-item'><a className='nav-link' href='#'>Contact</a></li>
+          </ul>
+        </div>
+
+      </nav>
+
+    
       
     </div>
   )
