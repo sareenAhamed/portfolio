d01dac09881fad11f3afa82c560cea195f67d99d
sareenAhamed <sareenahamed40@gmail.com>
2024-09-02 23:21:29 +0530

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import { Element } from 'react-scroll'
 
 const Footer = () => {
   return (
-    <Element name='footer'>
-      <div>
-        <h2>Footer</h2>
-      </div>
-    </Element>
+    <div className='container-fluid bg-slate-950 mt-12'>
+      
+      <p className='text-slate-400 text-center font-thin py-10 text-lg'>&copy; 2024 Sareen Ahamed. All Rights Reserved.</p>
+    </div>
+    
   )
 }
 
