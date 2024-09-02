a9ffc541fc91721984e5a3d88fe604b68d4eca77
sareenAhamed <sareenahamed40@gmail.com>
2024-09-02 17:31:55 +0530

    skills updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,6 +7,16 @@ import reactNative from '../assets/reactNative.png'
 import tailwind from '../assets/tailwind.png'
 import redux from '../assets/redux.png'
 import { Element } from 'react-scroll'
+import bootstrap from '../assets/bootstrap.png'
+import git from '../assets/git.png'
+import intellij from '../assets/intellij.png'
+import vscode from '../assets/vscode.png'
+import java from '../assets/java.png'
+import mysql from '../assets/mysql.png'
+
+
+
+
 
 
 const Skills = () => {
@@ -18,48 +28,73 @@ const Skills = () => {
           <hr />
         </div>
 
-        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+        <div className='grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6'>
           <div className='bg-slate-800 pt-5 m-3 rounded flex flex-col items-center pb-3 text-center'>
             <img className='w-14 h-14' src={html} alt="HTML 5" />
             <h2 className='mt-6 font-semibold text-portGreen text-xl mb-1'>HTML 5</h2>
-            <p className='text-slate-400'>It provides modern web structure, multimedia support, and enhanced performance.</p>
           </div>
 
           <div className='bg-slate-800 pt-5 m-3 rounded flex flex-col items-center pb-3 text-center'>
               <img className='w-14 h-14 ' src={css} alt="CSS 3" />
               <h2 className='mt-6 font-semibold text-portGreen text-xl mb-1'>CSS 3</h2>
-              <p className='text-slate-400'>It enables responsive design, advanced styling, and dynamic visual effects.</p>
           </div>
 
           <div className='bg-slate-800 pt-5 m-3 rounded flex flex-col items-center pb-3 text-center'>
             <img className='w-14 h-14 ' src={js} alt="JavaScript" />
             <h2 className='mt-6 font-semibold text-portGreen text-xl mb-1'>JavaScript</h2>
-            <p className='text-slate-400'>It powers interactivity, dynamic content, and real-time functionality on websites.</p>
           </div>
 
           <div className='bg-slate-800 pt-5 m-3 rounded flex flex-col items-center pb-3 text-center'>
             <img className='w-14 h-14' src={reactJs} alt="React JS" />     
             <h2 className='mt-6 font-semibold text-portGreen text-xl mb-1'>React JS</h2>
-            <p className='text-slate-400'>It simplifies building dynamic, reusable components and enhances performance.</p>
           </div>
 
           <div className='bg-slate-800 pt-5 m-3 rounded flex flex-col items-center pb-3 text-center'>
-          <img className='w-14 h-14 ' src={reactNative} alt="React Native" />
+            <img className='w-14 h-14 ' src={reactNative} alt="React Native" />
             <h2 className='mt-6 font-semibold text-portGreen text-xl mb-1'>React Native</h2>
-            <p className='text-slate-400'>It enables cross-platform mobile apps with a single codebase, ensuring fast development and native performance.</p>
           </div>
 
           <div className='bg-slate-800 pt-5 m-3 rounded flex flex-col items-center pb-3 text-center'>
-          <img className='w-14 h-14 ' src={tailwind} alt="Tailwind CSS" />
+            <img className='w-14 h-14 ' src={tailwind} alt="Tailwind CSS" />
             <h2 className='mt-6 font-semibold text-portGreen text-xl mb-1'>Tailwind CSS</h2>
-            <p className='text-slate-400'>It offers utility-first styling, rapid customization, and streamlined responsive design.</p>
           </div>
           
           <div className='bg-slate-800 pt-5 m-3 rounded flex flex-col items-center pb-3 text-center'>
-          <img className='w-14 h-14 ' src={redux} alt="Redux" />
-          <h2 className='mt-6 font-semibold text-portGreen text-xl mb-1'>Redux</h2>
-          <p className='text-slate-400'>It manages application state predictably, making complex state management simpler and more maintainable.</p>
+            <img className='w-14 h-14 ' src={redux} alt="Redux" />
+            <h2 className='mt-6 font-semibold text-portGreen text-xl mb-1'>Redux</h2>
           </div>
+
+          <div className='bg-slate-800 pt-5 m-3 rounded flex flex-col items-center pb-3 text-center'>
+            <img className='w-14 h-14 ' src={bootstrap} alt="Bootstrap" />
+            <h2 className='mt-6 font-semibold text-portGreen text-xl mb-1'>Bootstrap</h2>
+          </div>
+
+          <div className='bg-slate-800 pt-5 m-3 rounded flex flex-col items-center pb-3 text-center'>
+            <img className='w-14 h-14 ' src={mysql} alt="MySQL" />
+            <h2 className='mt-6 font-semibold text-portGreen text-xl mb-1'>MySQL</h2>
+          </div>
+
+          <div className='bg-slate-800 pt-5 m-3 rounded flex flex-col items-center pb-3 text-center'>
+            <img className='w-14 h-14 ' src={java} alt="Java" />
+            <h2 className='mt-6 font-semibold text-portGreen text-xl mb-1'>Java</h2>
+          </div>
+
+          <div className='bg-slate-800 pt-5 m-3 rounded flex flex-col items-center pb-3 text-center'>
+            <img className='w-14 h-14 ' src={vscode} alt="Vs Code" />
+            <h2 className='mt-6 font-semibold text-portGreen text-xl mb-1'>Vs Code</h2>
+          </div>
+
+          <div className='bg-slate-800 pt-5 m-3 rounded flex flex-col items-center pb-3 text-center'>
+            <img className='w-14 h-14 ' src={git} alt="Git" />
+            <h2 className='mt-6 font-semibold text-portGreen text-xl mb-1'>Git</h2>
+          </div>
+
+          <div className='bg-slate-800 pt-5 m-3 rounded flex flex-col items-center pb-3 text-center'>
+            <img className='w-14 h-14 ' src={intellij} alt="Intellij" />
+            <h2 className='mt-6 font-semibold text-portGreen text-xl mb-1'>Intellij</h2>
+          </div>
+
+          
         
 
         </div>
